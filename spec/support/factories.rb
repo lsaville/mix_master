bfb838b151e12f885b4c3f3dcdc8b9b1399ff752
Lee Saville <l.w.saville@gmail.com>
2016-10-19 16:42:02 -0600

    Pass user views all playlists spec

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,19 +1,21 @@
 FactoryGirl.define do
   factory :artist do
-    name
+    sequence :name do |n|
+      "#{n} Artist"
+    end
     image_path "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg"
   end
 
-  sequence :name do |n|
-    "#{n} Artist"
-  end
-
-  sequence :title, ["A", "C", "B"].cycle do |n|
-    "#{n} Title"
-  end
-
   factory :song do
-    title
+    sequence :title, ["A", "C", "B"].cycle do |n|
+      "#{n} Title"
+    end
     artist
+  end
+
+  factory :playlist do
+    sequence :name do |n|
+      "#{n} Playlist"
+    end
   end
 end

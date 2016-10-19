92c66f3e666a138cdb1ae8f75f3f473dac0af48e
Lee Saville <l.w.saville@gmail.com>
2016-10-19 16:51:22 -0600

    Pass user views all playlists spec

--- spec/features/user_views_all_playlists_spec.rb
+++ spec/features/user_views_all_playlists_spec.rb
@@ -3,11 +3,13 @@ require "rails_helper"
 RSpec.feature "User can see playlists and click on them" do
     scenario "they see playlist names" do
       playlist1, playlist2, playlist3 = create_list(:playlist, 3)
-
       visit playlists_path
 
       expect(page).to have_link playlist1.name
       expect(page).to have_link playlist2.name
       expect(page).to have_link playlist3.name
+
+      click_on playlist1.name
+      expect(page).to have_content playlist1.name
     end
 end
